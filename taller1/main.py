f5cb0544a6327516a7e43db7c978bb6188de9288
Theo <theoluis44@gmail.com>
2025-08-31 21:34:55 -0300

    cambio interno de la lógica de esPalabraLinda, build FUNCIONAL

--- taller1/main.py
+++ taller1/main.py
@@ -4,17 +4,17 @@ def recibirInput():
     for i in range(0,cantidadCasos):
         cantidadLetras = int(input()) #no me sirve realmente
         palabra = input()
-        palabras.append((cantidadLetras,palabra))
+        palabras.append(palabra)
     
-    return palabras #acá tengo que hacer el output que va a consola
+    return palabras
 
 
 def cantidadesDeCambios(palabras:list[str])->list[int]:
-    resultados = []
+    listaDeCambios = []
     for palabra in palabras:
-        resultados.append(str(esPalabraLinda(palabra,'a'))) #esto debería calcular cuantos cambios hay que hacerle a la palabra determinada
+        listaDeCambios.append(str(esPalabraLinda(len(palabra),palabra,'a'))) #esto debería calcular cuantos cambios hay que hacerle a la palabra determinada
     
-    return resultados
+    return listaDeCambios
 
 
 def esPalabraLinda(largo:int, palabra:str, letra:str) -> int: #aca decido si el string es l-lindo o no y devuelvo si hay que hacer un cambio o no en forma de int
@@ -29,19 +29,32 @@ def esPalabraLinda(largo:int, palabra:str, letra:str) -> int: #aca decido si el 
     mitadIzq: str = palabra[:mitad]
     mitadDer: str = palabra[mitad:]
 
-    if esPalabraLinda(mitadIzq, letra)==0 and esPalabraLinda(mitadDer, chr(ord(letra)+1))==0:
-        return cambios
-    elif esPalabraLinda(mitadDer, letra)==0 and esPalabraLinda(mitadIzq, chr(ord(letra)+1))==0:
-        return cambios
-    else: #no se bien que hacer aca, hay un problema con la logica
-        return cambios  
-        
+    letrasDistintasCaso1:int = 0
+    for l in mitadIzq:
+        if l != letra:
+            letrasDistintasCaso1 += 1
+    letrasDistintasCaso1 += esPalabraLinda(len(mitadDer), mitadDer, chr(ord(letra)+1))
+
+    letrasDistintasCaso2:int = 0
+    for l in mitadDer:
+        if l != letra:
+            letrasDistintasCaso2 += 1
+    letrasDistintasCaso2 += esPalabraLinda(len(mitadIzq), mitadIzq, chr(ord(letra)+1))
+
+    if letrasDistintasCaso1 <= letrasDistintasCaso2:
+        return letrasDistintasCaso1
+    else:
+        return letrasDistintasCaso2
+
 
 if __name__ == "__main__":
-    recibirInput()
+    strings = recibirInput()
+    output = cantidadesDeCambios(strings)
+    for i in output:
+        print(i)
 
 
-#esStringLindo('a', 'b')
+
 #Input
 '''
 6
@@ -69,34 +82,3 @@ ccddaabb
 1
 5
 '''
-
-
-################ PRUEBAS ################ 
-'''def esLindo(s:str, l:str )->bool: #s es el string y l es la letra que hace que el string sea l-lindo
-    if len(s) == 1 and s==l: #caso base
-        return True
-    
-    mitad: int = len(s) // 2 #div entera 
-    mitadIzq: str = s[:mitad]
-    mitadDer: str = s[mitad:]
-
-    if esLindo(mitadIzq, l) and esLindo(mitadDer, chr(ord(l)+1)):
-        return print((esLindo(mitadIzq, l) and esLindo(mitadDer, chr(ord(l)+1))))
-    elif esLindo(mitadDer, l) and esLindo(mitadIzq, chr(ord(l)+1)):
-        return print((esLindo(mitadIzq, l) and esLindo(mitadDer, chr(ord(l)+1))))
-    else: 
-        return False
-
-esLindo('aaaa','a')'''
-
-
-'''def cantidadDeCambios()->int: #aca tomo los valores input 
-    cuantasPalabras:str = input()
-    for i in range (0,2):
-
-    largoPalabra:int = input()
-    cambiosHechos:int = 0
-    
-    
-    
-    return 0'''
