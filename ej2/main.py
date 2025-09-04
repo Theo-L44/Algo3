7ae93b3e2fbab0995f088bb763bd795d9b4a5f82
Theo <theoluis44@gmail.com>
2025-09-04 20:33:49 -0300

    comienzo a definir funciones

--- ej2/main.py
+++ ej2/main.py
@@ -1,1 +1,17 @@
-#placeholder
+def recibirInput():
+    values = input().split()
+    valInicial = int(values[0])
+    valObjetivo = int(values[1])
+    
+    
+    return #print(valInicial+', '+valObjetivo)
+
+
+
+
+#recibirInput('1  4')
+
+if __name__ == "__main__":
+    values = recibirInput()
+    #output = cantidadesDeCambios(strings)
+    print('ok')
