bd04a192416dd93f3ff2644e300a5c9f2c006d73
Theo <theoluis44@gmail.com>
2025-09-05 20:48:56 -0300

    continuo definiendo funciones

--- ej3/main.py
+++ ej3/main.py
@@ -1,8 +1,11 @@
 def recibirInput()->list[int]: #recibo el input puesto en consola
     input:list[int] = int(input().split())
-    numeroAsqueroso:int = int(input[0])
+    numeroAsqueroso:list[int] = [int(input[0])]
     rango:list[int] = [input[1],input[2]]
 
+    if numeroAsqueroso[0] != 1:
+        numeroAsqueroso = [numeroAsqueroso[0]//2, numeroAsqueroso[0]%2, numeroAsqueroso[0]//2]
+        
     listaDeUnos:list[int] = convertirEnUnos(numeroAsqueroso)
 
     cantidadDeUnos = 0
@@ -13,15 +16,18 @@ def recibirInput()->list[int]: #recibo el input puesto en consola
 
     return cantidadDeUnos
 
-def convertirEnUnos(n:int)->list[int]:
-    listaDeUnos:list[int] = [n]
+def convertirEnUnos(n:list[int])->list[int]:
+    listaDeUnos:list[int] = n
+
+    mitad: int = len(listaDeUnos) // 2 #calculo donde esta la mitad del string para separar las dos mitades y hacer DyC
+    mitadIzq: str = listaDeUnos[:mitad] 
+    mitadDer: str = listaDeUnos[mitad:]
 
     for numero in listaDeUnos:
         if numero != 1 or numero!=0:
             convertirEnUnos(numero)
 
     return listaDeUnos
-
 
 if __name__ == "__main__":
     numero = recibirInput() #recibo las palabras junto con sus largos
