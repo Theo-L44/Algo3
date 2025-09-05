268e10aeb37629258905968d5a0de9decf3032a0
Theo <theoluis44@gmail.com>
2025-09-05 19:20:56 -0300

    Empiezo a definir funciones para main.py en ej3

--- ej3/main.py
+++ ej3/main.py
@@ -1,4 +1,24 @@
+def recibirInput()->list[int]: #recibo el input puesto en consola
+    input = int(input().split)
+    numeroAsqueroso = input[0]
+    rango = [input[1],input[2]]
+
+    listaDeUnos:list[int] = convertirEnUnos(numeroAsqueroso)
+
+    cantidadDeUnos = 0
+
+    for i in range(rango[0], rango[1]+1): 
+        if listaDeUnos[i] == 1:
+            cantidadDeUnos += 1
+
+    return cantidadDeUnos
+
+def convertirEnUnos(n:int)->list[int]:
+
+    
+
+    return []
 if __name__ == "__main__":
-    strings = recibirInput() #recibo las palabras junto con sus largos
+    numero = recibirInput() #recibo las palabras junto con sus largos
     output = 0
-    return output
+    print(output)
