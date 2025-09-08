8448c281a1a8b895c0054f1c0fb8a119a1f0e6c2
Theo <theoluis44@gmail.com>
2025-09-08 13:47:11 -0300

    elimino identaciones y espacios de más

--- ej2/main.py
+++ ej2/main.py
@@ -17,7 +17,6 @@ def recibirInput()->str:
 
             if n==str(movimientos[len(movimientos)-1]): #tengo que hacer esto para que no me agregue una identación al principio
                 resultado = resultado + n 
-
             else:
                 resultado += ' ' + n
 
@@ -32,10 +31,8 @@ def listarMovimientos(valorInicial:int, valorObjetivo:int)->list[int]:
 
         if valorActual % 2 == 0: #si el valor es par significa que provino de hacer la multiplicacion por 2
             valorActual = int(valorActual//2) #comprobe que es par, asi que divido por 2 en division entera 
-
         elif (valorActual)%10 == 1: #Si divido por 10 y el resto es uno, entonces el numero pudo haber provenido de haber hecho n*10 + 1
             valorActual = int((valorActual-1)//10) #comprobe que podia restar 1 y dividir por 10
-
         else: #el valor en el que estoy parado no es par ni tampoco tiene un uno a la izq, por lo que no puedo formar el valorInicial
             movimientos = [] #no se puede formar la cadena de int que me lleva a mi resultado objetivos, por lo que ya se que la respuesta es no
             return movimientos
@@ -43,7 +40,7 @@ def listarMovimientos(valorInicial:int, valorObjetivo:int)->list[int]:
     if valorActual==valorInicial:
         movimientos.append(valorActual) #llegué al valor inicial
         return movimientos    
-            
+
     return movimientos
 
 if __name__ == "__main__":
