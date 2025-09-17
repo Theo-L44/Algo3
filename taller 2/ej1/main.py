d7e2b751fac1b05824c4115d3c7eb892ff2a322b
Theo <theoluis44@gmail.com>
2025-09-17 20:59:44 -0300

    defino recibirInput

--- taller 2/ej1/main.py
+++ taller 2/ej1/main.py
@@ -1,14 +1,29 @@
 memoria = [] 
 
 def recibirInput():
-    _ = input().split()
-    for i in _:
-        True
+    n = int(input().strip()) #string entero
+    
+    costos = input().split() #me devuelve la linea de los costos 
+    
+    for i in range(n):
+        costos[i] = int(costos[i])
 
-    respuesta = True
+    palabras = []
+    
+    for i in range(n):
+        palabras.append(input().strip())
+
+    respuesta = calcularCambios(n,costos,palabras)
+
     return respuesta
  
+
+def calcularCambios(n,costos,palabras):
+    return True
+
 if __name__ == "__main__":
-    output = recibirInput()
-    for i in output:
-        print(i)
+    movimientos = recibirInput()
+    print(movimientos)
+
+#para dar vuelta un string uso reversed_string = my_string[::-1] entonces paso de 'hola' a 'aloh'
+#para obtener el valor de una letra (a=1, b=2, etc), uso 
