fd1f5b4a9ad859b75fd2f040da752a64d8e11e3d
Theo <theoluis44@gmail.com>
2025-09-18 17:23:18 -0300

    Encontré la manera de iniciar la memoria

--- taller 2/ej1/main.py
+++ taller 2/ej1/main.py
@@ -1,8 +1,5 @@
-memoria = [] 
-
 def recibirInput():
     n = int(input().strip()) #string entero
-    
     costos = input().split() #me devuelve la linea de los costos 
     
     for i in range(n):
@@ -19,11 +16,22 @@ def recibirInput():
  
 
 def calcularCambios(n,costos,palabras):
-    return True
+    memoria = [[invalido,invalido] for i in range(n)] #inicio la memoria
+    i = 0
+    cambios = 0
+    invalido = 10**10
+
+
+
+    #    reverso = palabras[i][::-1]
+    #    memoria.append(reverso)
+    #    i+1
+    
+    return cambios
 
 if __name__ == "__main__":
     movimientos = recibirInput()
     print(movimientos)
 
-#para dar vuelta un string uso reversed_string = my_string[::-1] entonces paso de 'hola' a 'aloh'
+#para dar vuelta un string uso reversed_string = my_string[::-1] entonces pasa de 'hola' a 'aloh'
 #para obtener el valor de una letra (a=1, b=2, etc), uso 
