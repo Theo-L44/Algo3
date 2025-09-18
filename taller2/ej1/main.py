be7e594fe97b42943dc3b4c4c3f0183ae38dba28
Theo <theoluis44@gmail.com>
2025-09-18 20:48:30 -0300

    Completo ejercicio 1

--- taller2/ej1/main.py
+++ taller2/ej1/main.py
@@ -14,7 +14,6 @@ def recibirInput():
 
     return respuesta
  
-
 def calcularCambios(n,costos,palabras):
     invalido = 10**10
     cambios = 0
@@ -32,17 +31,17 @@ def calcularCambios(n,costos,palabras):
 
         #primero veo el caso revirtiendo la palabra anterior
         if palabras[i-1] <= palabras[i]:
-            memoria[i][0] = min(memoria[i][0], memoria[i-1][0]) #veo si es menor que la palabra anterior 
+            memoria[i][0] = min(memoria[i][1], memoria[i-1][0]) #veo si es menor que la palabra anterior 
         if revertidaAnterior <= palabras[i]:
-            memoria[i][0] = min(memoria[i][0], memoria[i-1][1])
+            memoria[i][0] = min(memoria[i][1], memoria[i-1][1])
 
         revertida = palabras[i][::-1] #ahora revierto la palabra actual
 
         #ahora veo que pasa si tengo la palabra actual revertida    
         if palabras[i-1] <= revertida:
-            memoria[i][1] = min(memoria[i][0], memoria[i-1][0] + costos[i])
+            memoria[i][1] = min(memoria[i][1], memoria[i-1][0] + costos[i])
         if revertidaAnterior <= revertida:
-            memoria[i][1] = min(memoria[i][0], memoria[i-1][1] + costos[i])
+            memoria[i][1] = min(memoria[i][1], memoria[i-1][1] + costos[i])
 
     cambios = min(memoria[n-1][0], memoria[n-1][1])
 
@@ -57,3 +56,31 @@ if __name__ == "__main__":
 
 #para dar vuelta un string uso reversed_string = my_string[::-1] entonces pasa de 'hola' a 'aloh'
 #para obtener el valor de una letra (a=1, b=2, etc), uso 
+
+#>Input
+# 3
+# 100 200 300
+# abc
+# efg
+# hij
+
+# >Output
+# 0
+
+# >Input
+# 3
+# 100 200 300
+# za
+# yb
+# xc
+
+# >Output
+# 300
+
+# >Input
+# 2
+# 0 0
+# casas
+# abraza
+# >Output
+# -1
