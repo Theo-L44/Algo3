589340d857c42b720a58ae978e6fdb8e21f63cdd
Theo <theoluis44@gmail.com>
2025-09-19 21:47:25 -0300

    continuo definiendo cosas

--- taller2/ej2/main.py
+++ taller2/ej2/main.py
@@ -1,5 +1,17 @@
 def recibirInput():
-    return 1
+    n:int = int(input().strip())
+    palabra:str = input().split()
+
+    respuesta:int = cantidadSubsecuencias(n, palabra)
+    return respuesta
+
+def cantidadSubsecuencias(n:int, palabra:str):
+    invalido:int = 10**15
+    cambios:int = 0
+
+    memoria = []
+    
+    return 1 
 
 if __name__ == "__main__":
     cantOperaciones = recibirInput()
