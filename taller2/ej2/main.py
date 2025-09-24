27842eb497bf49746d7885534dbd537e5f34c319
Theo <theoluis44@gmail.com>
2025-09-24 19:09:37 -0300

    completado con errores

--- taller2/ej2/main.py
+++ taller2/ej2/main.py
@@ -1,22 +1,36 @@
 def recibirInput():
     n:int = int(input().strip()) #longitud del string
     palabra:str = input().strip()
+    
+    memoria = [] #inicio la memoria
+    invalido = 10**15
 
-    respuesta:int = cantidadSubsecuencias(n, palabra)
-    return respuesta
+    for _ in range(n):
+        memoria.append([invalido]*n) #creo la matriz
+    
+    resultado = cantidadOperaciones(0, n - 1, palabra, memoria)
+    return resultado
 
-def cantidadSubsecuencias(n:int, palabra:str):
-    invalido:int = 10**15
-    memoria = []
+def cantidadOperaciones(i, j, palabra, memoria): #abcba
+    if i > j:
+        return 0 #no tengo mas palabra
+    
+    if memoria[i][j] != -1:
+        return memoria[i][j] #ya tengo el resultado, no lo vuelvo a calcular
+    
+    if i == j:
+        memoria[i][j] = 1 #caso base en el que tengo solo 1 letra
+        return 1
 
-    for i in range(n):
-        memoria.append([invalido]*n) #revisar, creo la matriz
-
-    for i in range(n): 
-        memoria[i][i] = 1
-
-    return 1 #placeholder
+    operaciones = 1 + cantidadOperaciones(i + 1, j, palabra, memoria) 
+    
+    for k in range(i + 1, j + 1): #busco letras iguales asi no las elimino, y quedan para el final
+        if palabra[i] == palabra[k]:
+            operaciones = min(operaciones, cantidadOperaciones(i + 1, k - 1, palabra, memoria) + cantidadOperaciones(k, j, palabra, memoria))
+    
+    memoria[i][j] = operaciones #escribo la memoria
+    return operaciones
 
 if __name__ == "__main__":
-    cantOperaciones = recibirInput()
-    print(cantOperaciones)
+    resultado = recibirInput()
+    print(resultado)
