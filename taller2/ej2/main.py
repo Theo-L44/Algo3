971cc0282927bc1c3c670b67fa5ac46572303227
Theo <theoluis44@gmail.com>
2025-09-24 21:52:03 -0300

    agrego comentarios

--- taller2/ej2/main.py
+++ taller2/ej2/main.py
@@ -6,6 +6,7 @@ def recibirInput():
     invalido = 10**15
     if n == 0:
         return 0
+    
     for _ in range(n):
         memoria.append([invalido]*n) #creo la matriz con valores invalidos
     
@@ -16,14 +17,14 @@ def cantidadOperaciones(i, j, palabra, memoria):
     if i > j:
         return 0 #no tengo mas palabra (los rangos son invalidos)
     
-    if memoria[i][j] != 10**15:
-        return memoria[i][j] #ya tengo el resultado, no lo vuelvo a calcular
+    if memoria[i][j] != 10**15: #ya tengo el resultado (veo si la matriz tiene un valor distinto "invalido"), no lo vuelvo a calcular
+        return memoria[i][j] 
     
     if i == j:
         memoria[i][j] = 1 #caso en el que tengo solo 1 letra
         return 1
 
-    operaciones = 1 + cantidadOperaciones(i + 1, j, palabra, memoria) 
+    operaciones = 1 + cantidadOperaciones(i + 1, j, palabra, memoria) #lo que me cuesta borrar todos los caracteres uno a uno
     
     for k in range(i + 1, j + 1):
         if palabra[i] == palabra[k]:
@@ -31,7 +32,7 @@ def cantidadOperaciones(i, j, palabra, memoria):
             faltante = cantidadOperaciones(k, j, palabra, memoria) #string que me falta calcular
             costo = medio + faltante 
 
-            if costo < operaciones:
+            if costo < operaciones: #veo si el nuevo costo es menor a la cantidad de operaciones que ya tengo
                 operaciones = costo
     
     memoria[i][j] = operaciones #reescribo la memoria
