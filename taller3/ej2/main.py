678b649f80d75e1f16d64afa4fa913d7cf89bea5
Theo <theoluis44@gmail.com>
2025-10-30 17:04:52 -0300

    Esqueletos de funciones para cada ejercicio de taller 3

---
+++ taller3/ej2/main.py
@@ -0,0 +1,10 @@
+def recibirInput():
+    n:int = (input().strip()) 
+    return True
+
+def resolver():
+    return True
+
+if __name__ == "__main__":
+    respuesta = recibirInput()
+    print(respuesta)
