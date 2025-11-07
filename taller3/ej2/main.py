43e28d64d773b1fcfefdf8138e97dc14575ac63d
Theo <theoluis44@gmail.com>
2025-11-07 19:33:18 -0300

    Defino algunas cosas Ej2 - taller 3

--- taller3/ej2/main.py
+++ taller3/ej2/main.py
@@ -1,10 +1,30 @@
-def recibirInput():
-    n:int = (input().strip()) 
-    return True
+import heapq
 
-def resolver():
-    return True
+def recibirInput():
+    datos = input().split()
+    cantPlanetas:int = int(datos[0])
+    cantPortales:int = int(datos[1])
+    grafo = []
+
+    for i in range(2,cantPortales+1):
+        
+
+
+    res:int = contarTiempo(grafo)
+    
+    return res
+
+def contarTiempo(grafo):
+    
+    tiempoTotal:int = 0
+    
+    grafo.append(0)
+    calculados = set()
+    calculados.add(0)
 
 if __name__ == "__main__":
-    respuesta = recibirInput()
+    respuesta = str(recibirInput())
     print(respuesta)
+
+
+#usar .heapify() para transformar una lista en un min-heap
