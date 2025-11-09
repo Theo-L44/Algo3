c1c3aab83b220be76b14972f8bf6ad153c2561dc
Theo <theoluis44@gmail.com>
2025-11-09 16:12:39 -0300

    for para completar el grafo

--- taller3/ej2/main.py
+++ taller3/ej2/main.py
@@ -4,13 +4,22 @@ def recibirInput():
     datos = input().split()
     cantPlanetas:int = int(datos[0])
     cantPortales:int = int(datos[1])
-    grafo = []
+    portales = []
 
-    for i in range(2,cantPortales+1):
-        
+    for _ in range(cantPlanetas+1):
+        portales.append([])
 
+    for _ in range(cantPortales):
+        datos = input().split()
+        planetaInicio = datos[0]
+        planetaDestino = datos[1]
+        tiempo = datos[2]
+        portales[planetaInicio].append((planetaDestino,tiempo)) #agrego en la posición "planetaInicio" los planetas a los que pouedo llegar desde ese planeta como una tupla planeta/tiempo
+        portales[planetaDestino].append((planetaInicio,tiempo)) #como son bidireccionales los portales, agrego en la posicion 
+    
+    
 
-    res:int = contarTiempo(grafo)
+    res:int = contarTiempo(portales)
     
     return res
 
