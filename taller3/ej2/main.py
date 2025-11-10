6e83d7cf05a8c4575d84ab74d1d7c47ae9cf5b23
Theo <theoluis44@gmail.com>
2025-11-09 23:46:01 -0300

    defino algunas funciones de contarTiempo

--- taller3/ej2/main.py
+++ taller3/ej2/main.py
@@ -4,44 +4,53 @@ def recibirInput():
     datos = input().split()
     cantPlanetas:int = int(datos[0])
     cantPortales:int = int(datos[1])
-    portales = []
+    galaxia = []
 
     for _ in range(cantPlanetas+1):
-        portales.append([])
+        galaxia.append([])
 
     for _ in range(cantPortales):
         datos = input().split()
-        planetaInicio = datos[0]
-        planetaDestino = datos[1]
+        planetaInicio = int(datos[0])
+        planetaDestino = int(datos[1])
         tiempo = datos[2]
-        portales[planetaInicio].append((planetaDestino,tiempo)) #agrego en la posición "planetaInicio" los planetas a los que pouedo llegar desde ese planeta como una tupla planeta/tiempo
-        portales[planetaDestino].append((planetaInicio,tiempo)) #como son bidireccionales los portales, agrego en la posicion 
+        galaxia[planetaInicio].append((planetaDestino,tiempo)) #agrego en la posición "planetaInicio" los planetas a los que pouedo llegar desde ese planeta como una tupla planeta/tiempo
+        galaxia[planetaDestino].append((planetaInicio,tiempo)) #como son bidireccionales los portales, agrego en la posicion 
     
     viajeros = []
     for _ in range(cantPlanetas):
         datos = input().split()
         cantViajeros = int(datos[0]) #cantidad de viajeros que van a ocupar un portal de un planeta
         minutoEspera = [] #tiempos en los que aparecen los otros viajeros en los portales
-        for n in range(1, n+1):
+        for n in range(1, cantViajeros+1):
             minutoEspera.append(int(datos[n]))
         viajeros.append(minutoEspera)
 
 
-    res:int = contarTiempo(cantPlanetas, portales, viajeros)
+    res:int = contarTiempo(cantPlanetas, galaxia, viajeros)
     
     return res
 
-def contarTiempo(cantPlanetas, grafo, llegadas):
+def contarTiempo(cantPlanetas, galaxia, llegadas):
     
-    tiempoTotal:int = 0
+    tiempoTotal:int = -1 #no se puede llegar al planeta destino
+
+    tiempoPortal = []
     
-    grafo.append(0)
-    calculados = set()
-    calculados.add(0)
+    for _ in range(0,cantPlanetas+1):
+        tiempoPortal.append([10**18]) #propongo un numero enorme para despues comparar
+
+    tiempoPortal[1]=0
+
+    planetasSinExplorar  = [(0,1)] #(tiempo, planeta), el planeta 1 siempre tiene tiempo 0
+
+    while len(planetasSinExplorar) != 0:
+        break
+
+
+    return tiempoTotal
+
 
 if __name__ == "__main__":
     respuesta = str(recibirInput())
     print(respuesta)
-
-
-#usar .heapify() para transformar una lista en un min-heap
